1eae1abce7a1a47cfb121f00fcf29f4c84d92a00
Mingjie Jiang <jiang@mingjie.info>
2020-02-07 23:43:25 -0500

    Minor cleanup

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const airtable = require("airtable");
-const express = require("express");
-const app = express();
+const airtable = require("airtable")
+const express = require("express")
+const app = express()
 
 // Load Environment Variables
-require("dotenv").config();
+require("dotenv").config()
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3000
 
-app.listen(port, () => console.log(`Snaps is running on port ${port}.`));
+app.listen(port, () => console.log(`Snaps is running on port ${port}.`))
 
-app.get("/ping", (_, res) => res.sendStatus(200));
+app.get("/ping", (_, res) => res.sendStatus(200))
 
 // Post new snaps to server
 app.post("/snap", (req, res) => {
-    
+
 })
